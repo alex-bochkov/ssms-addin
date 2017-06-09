13e991c4df36d96aed690b3034d744c9067b9139
Alexey Bochkov <albochkov03@gmail.com>
2017-06-09 09:59:35 -0700

    Update Last restored log files SQL2008.sql

--- SSMS_Tool/QueryTemplates/Replication/Last restored log files SQL2008.sql
+++ SSMS_Tool/QueryTemplates/Replication/Last restored log files SQL2008.sql
@@ -1,8 +1,10 @@
 SELECT SD.[secondary_database],
-       SD.[last_restored_file],
+       LEFT(RIGHT(SD.last_restored_file, CHARINDEX('\', REVERSE(SD.last_restored_file)) - 1), 50) AS LastRestoredFile,
+       MS.last_copied_date,
        SD.[last_restored_date],
        MS.last_restored_latency,
-       MS.last_copied_date
+       DATEADD(minute, -MS.last_restored_latency + 7 * 60, SD.[last_restored_date]) AS DatabaseTimestamp,
+       DATEDIFF(minute, DATEADD(minute, -MS.last_restored_latency + 7 * 60, SD.[last_restored_date]), GETDATE()) AS ActualLatency
 FROM [msdb].[dbo].[log_shipping_secondary_databases] AS SD
      INNER JOIN
      [msdb].[dbo].[log_shipping_monitor_secondary] AS MS
