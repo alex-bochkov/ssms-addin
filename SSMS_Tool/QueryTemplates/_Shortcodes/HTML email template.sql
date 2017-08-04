53f515445e48e4f943822f1f163e2a434f50d320
Alexey Bochkov <albochkov03@gmail.com>
2017-08-04 14:38:24 -0700

    Update HTML email template.sql

--- SSMS_Tool/QueryTemplates/_Shortcodes/HTML email template.sql
+++ SSMS_Tool/QueryTemplates/_Shortcodes/HTML email template.sql
@@ -1,16 +1,22 @@
 DECLARE @tableHTML VARCHAR(max)
 	SET @tableHTML =
-		N'<H3>Mismatched AMEX processor</H3>' +
+		N'<H3> - TITLE - </H3>' +
 		N'<em>' + CONVERT(NVARCHAR, GETDATE(), 120) + N'</em>' +
 		N'<table border="1">' +
-		N'<tr> <th>Platform</th> <th>ID</th> <th>Name</th> <th>Ref</th> <th>Amount</th> </tr>' +
+		N'<tr> 
+			<th>Platform</th> 
+			<th>ID</th> 
+			<th>Name</th> 
+			<th>Ref</th> 
+			<th>Amount</th> 
+		</tr>' +
 		CAST ( ( select 
 			  td = [platform], '',
 			  td = id, '',
 			  td = name, '',
 			  td = Ref, '',
 			  td = Amount
-			from ##ReportMismatchedAmexProcessor
+			from ##temptable
 			FOR XML PATH('tr'), TYPE 
 		) AS NVARCHAR(MAX) ) +
 		N'</table>' ;
@@ -18,13 +24,7 @@ DECLARE @tableHTML VARCHAR(max)
 		
 EXEC msdb.dbo.sp_send_dbmail 
 		@profile_name = 'DBA',
-		@recipients = 'techsupport@yapstone.com',
-		@subject = 'Mismatched Amex processor',
+		@recipients = 'dba@google.com',
+		@subject = 'Email title',
 		@body = @tableHTML,
-		@body_format = 'HTML', 
-		@query = @qry,
-		 @attach_query_result_as_file = 1,
-		 @query_attachment_filename = 'MismatchedAmexProcessor.csv',
-		 @query_result_separator = ',',
-		 @query_result_width = 32767,
-		 @query_result_no_padding = 1;
+		@body_format = 'HTML';
