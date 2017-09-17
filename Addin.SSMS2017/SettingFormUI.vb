c7464ad3ee37ef80e71e3b4a7b92a64b84e01834
Alex Bochkov <albochkov03@gmail.com>
2017-09-17 11:33:41 -0700

    new version

---
+++ Addin.SSMS2017/SettingFormUI.vb
@@ -0,0 +1,20 @@
+﻿Public Class SettingFormUI
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        TemplateScriptFolderTextBox.Text = SettingManager.GetTemplatesFolder()
+        ExcelExportFolderTextBox.Text = SettingManager.GetExcelExportFolder()
+        TSQLFormatVersion.SelectedItem = SettingManager.GetTSQLFormatVersion
+
+    End Sub
+
+    Private Sub SaveButon_Click(sender As Object, e As System.EventArgs) Handles SaveButon.Click
+
+        SettingManager.SaveTemplatesFolder(TemplateScriptFolderTextBox.Text)
+        SettingManager.SaveExcelExportFolder(ExcelExportFolderTextBox.Text)
+        SettingManager.SaveTSQLFormatVersion(TSQLFormatVersion.SelectedItem)
+
+    End Sub
+End Class
