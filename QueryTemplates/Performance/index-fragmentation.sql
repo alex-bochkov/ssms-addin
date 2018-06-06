67882e434729a42c5a3728cb61d16d607c3e77f4
Alexey Bochkov <albochkov03@gmail.com>
2018-06-06 12:46:07 -0700

    Update index-fragmentation.sql

--- QueryTemplates/Performance/index-fragmentation.sql
+++ QueryTemplates/Performance/index-fragmentation.sql
@@ -3,7 +3,8 @@ SELECT dbschemas.[name] as 'Schema',
 	dbindexes.[name] as 'Index',
 	indexstats.avg_fragmentation_in_percent,
 	indexstats.page_count,
-	'ALTER INDEX ['+dbindexes.[name]+'] ON [' + dbschemas.name + '].['+dbtables.[name]+'] REORGANIZE;'
+	'ALTER INDEX ['+dbindexes.[name]+'] ON [' + dbschemas.name + '].['+dbtables.[name]+'] REORGANIZE;',
+	'ALTER INDEX ['+dbindexes.[name]+'] ON [' + dbschemas.name + '].['+dbtables.[name]+'] REBUILD WITH (FILLFACTOR = 90, ONLINE = ON, DATA_COMPRESSION = PAGE, SORT_IN_TEMPDB = ON);'
 FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, NULL) AS indexstats
 INNER JOIN sys.tables dbtables on dbtables.[object_id] = indexstats.[object_id]
 INNER JOIN sys.schemas dbschemas on dbtables.[schema_id] = dbschemas.[schema_id]
