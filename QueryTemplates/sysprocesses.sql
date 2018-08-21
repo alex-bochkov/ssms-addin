185af92196c352bf67818c525394775c78b37452
Alexey Bochkov <albochkov03@gmail.com>
2018-08-21 16:12:41 -0700

    Update sysprocesses.sql

--- QueryTemplates/sysprocesses.sql
+++ QueryTemplates/sysprocesses.sql
@@ -10,11 +10,12 @@ SELECT ec.client_net_address,
        es.[program_name],
        es.[host_name],
        es.login_name,
+       DB_NAME(es.database_id) as DatabaseName,
        COUNT(ec.session_id) AS [connection count]
 FROM sys.dm_exec_sessions AS es WITH (NOLOCK)
      INNER JOIN
      sys.dm_exec_connections AS ec WITH (NOLOCK)
      ON es.session_id = ec.session_id
-GROUP BY ec.client_net_address, es.[program_name], es.[host_name], es.login_name
+GROUP BY ec.client_net_address, es.[program_name], es.[host_name], es.login_name, DB_NAME(es.database_id)
 ORDER BY ec.client_net_address, es.[program_name]
 OPTION (RECOMPILE);
