2bb10536d679d3b97768fa4585dcbf7692f4888f
Alexey Bochkov <albochkov03@gmail.com>
2019-03-19 11:29:57 -0700

    Update partitions-stats.sql

--- QueryTemplates/Common Scripts/partitions-stats.sql
+++ QueryTemplates/Common Scripts/partitions-stats.sql
@@ -12,7 +12,8 @@ SELECT
       CASE pf.boundary_value_on_right
             WHEN 1 THEN 'RIGHT'
             ELSE 'LEFT' END    AS Range,
-      p.rows AS Rows
+      p.rows AS [Rows],
+      p.[data_compression_desc] AS [Compression]
 FROM sys.partitions                  AS p
 JOIN sys.indexes                     AS i
       ON i.object_id = p.object_id
@@ -39,32 +40,9 @@ LEFT JOIN sys.partition_range_values AS prv_right
 WHERE
       OBJECTPROPERTY(p.object_id, 'ISMSShipped') = 0
       AND p.index_id = 1 /* only show clustered index */
-UNION ALL
---non-partitioned table/indexes
-SELECT
-      SCHEMA_NAME(o.schema_id)    AS SchemaName,
-      OBJECT_NAME(p.object_id)    AS ObjectName,
-      i.name                      AS IndexName,
-      p.index_id                  AS IndexID,
-      NULL                        AS PartitionScheme,
-      p.partition_number          AS PartitionNumber,
-      fg.name                     AS FileGroupName,  
-      NULL                        AS LowerBoundaryValue,
-      NULL                        AS UpperBoundaryValue,
-      NULL                        AS Boundary, 
-      p.rows                      AS Rows
-FROM sys.partitions     AS p
-JOIN sys.indexes        AS i
-      ON i.object_id = p.object_id
-      AND i.index_id = p.index_id
-JOIN sys.tables		AS o
-	  ON i.object_id = o.object_id
-JOIN sys.data_spaces    AS ds
-      ON ds.data_space_id = i.data_space_id
-JOIN sys.filegroups           AS fg
-      ON fg.data_space_id = i.data_space_id
-WHERE
-      OBJECTPROPERTY(p.object_id, 'ISMSShipped') = 0
+      --AND OBJECT_NAME(p.object_id) = ''
+      --AND i.name = ''
+      --AND p.partition_number = 0
 ORDER BY
       ObjectName,
       IndexID,
