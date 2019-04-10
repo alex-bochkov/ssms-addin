d2d49ce3b09527290a27a0a1add38a00ef81e1e5
Alexey Bochkov <albochkov03@gmail.com>
2019-04-10 16:02:42 -0700

    Update grow-log-file.sql

--- QueryTemplates/Common Scripts/grow-log-file.sql
+++ QueryTemplates/Common Scripts/grow-log-file.sql
@@ -1,3 +1,8 @@
+/*
+ Keep in mind that until certain point SQL servers creates 16 VLF for each growth operation.
+ To make sure that VLFs are large from the beginning grow it a LOT first time.
+*/
+
 DECLARE @INT INT = 2, @Cmd VARCHAR(max);
 --target size 100GB
 WHILE @INT <= 100
@@ -10,3 +15,21 @@ BEGIN
 	SET @Int += 1;
 
 END
+
+
+select 
+  li.VLF, 
+  mf.growth, 
+  mf.* 
+from sys.master_files mf 
+cross apply (
+    SELECT 
+      COUNT(*) AS VLF 
+    FROM 
+      sys.dm_db_log_info(mf.database_id) li
+  ) AS li 
+where 
+  mf.type = 1 
+  and mf.database_id > 4 
+order by 
+  mf.growth
