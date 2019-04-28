febfec92e4423b7745eb3d4cb638daff172341b8
Alexey Bochkov <albochkov03@gmail.com>
2019-04-28 12:27:05 -0700

    Create untrusted-constraints.sql

---
+++ QueryTemplates/Common Scripts/untrusted-constraints.sql
@@ -0,0 +1,12 @@
+SELECT '[' + s.name + '].[' + o.name + '].[' + i.name + ']' AS keyname
+from sys.foreign_keys i
+INNER JOIN sys.objects o ON i.parent_object_id = o.object_id
+INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+WHERE i.is_not_trusted = 1 AND i.is_not_for_replication = 0;
+GO
+SELECT '[' + s.name + '].[' + o.name + '].[' + i.name + ']' AS keyname
+from sys.check_constraints i
+INNER JOIN sys.objects o ON i.parent_object_id = o.object_id
+INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+WHERE i.is_not_trusted = 1 AND i.is_not_for_replication = 0 AND i.is_disabled = 0;
+GO
