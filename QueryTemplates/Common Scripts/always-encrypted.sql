1f60c1e8e1b9185638532c99a6d94aa44ce114f0
Alex Bochkov <albochkov03@gmail.com>
2020-09-02 13:39:33 -0700

    Update always-encrypted.sql

--- QueryTemplates/Common Scripts/always-encrypted.sql
+++ QueryTemplates/Common Scripts/always-encrypted.sql
@@ -1,13 +1,21 @@
-SELECT t.name AS TableName,
+SELECT CONCAT(s.name, '.', t.name) AS TableName,
        c.name AS ColumnName,
-       k.name AS KeyName,
-       c.encryption_type_desc,
-       c.encryption_algorithm_name
+       ty.Name + '(' + CAST (c.max_length AS VARCHAR (10)) + ')' AS ColumnType,
+       cmk.[name] AS CMK_name,
+       cmk.create_date AS CMK_createDate,
+       k.[name] AS CEK_name,
+       c.encryption_type_desc
 FROM sys.columns AS c
-     INNER JOIN
-     sys.column_encryption_keys AS k
-     ON c.column_encryption_key_id = k.column_encryption_key_id
-     INNER JOIN
-     sys.tables AS t
-     ON c.object_id = t.object_id
+     INNER JOIN sys.types AS ty
+		ON c.user_type_id = ty.user_type_id
+     INNER JOIN sys.column_encryption_keys AS k
+		ON c.column_encryption_key_id = k.column_encryption_key_id
+     INNER JOIN sys.tables AS t
+		ON c.object_id = t.object_id
+     INNER JOIN sys.schemas AS s
+		ON t.schema_id = s.schema_id
+     INNER JOIN sys.column_encryption_key_values AS cekv
+		ON cekv.column_encryption_key_id = k.column_encryption_key_id
+     INNER JOIN sys.column_master_keys AS cmk
+		ON cekv.column_master_key_id = cmk.column_master_key_id
 WHERE encryption_type IS NOT NULL;
