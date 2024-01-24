7996ec74949c4bc48a0b684912fdfa973fad8368
Alex Bochkov <albochkov03@gmail.com>
2024-01-23 16:26:25 -0800

    Update disk-info.sql

--- QueryTemplates/disk-info.sql
+++ QueryTemplates/disk-info.sql
@@ -1,9 +1,10 @@
-SELECT DISTINCT vs.volume_mount_point, vs.file_system_type, vs.logical_volume_name, 
-CONVERT(DECIMAL(18,2), vs.total_bytes/1073741824.0) AS [Total Size (GB)],
-CONVERT(DECIMAL(18,2), vs.available_bytes/1073741824.0) AS [Available Size (GB)],  
-CONVERT(DECIMAL(18,2), vs.available_bytes * 1. / vs.total_bytes * 100.) AS [Space Free %],
-vs.supports_compression, vs.is_compressed, 
-vs.supports_sparse_files, vs.supports_alternate_streams
-FROM sys.master_files AS f WITH (NOLOCK)
-CROSS APPLY sys.dm_os_volume_stats(f.database_id, f.[file_id]) AS vs 
-ORDER BY vs.volume_mount_point OPTION (RECOMPILE);
+SELECT DISTINCT 
+	vs.volume_mount_point,
+	vs.file_system_type,
+	vs.logical_volume_name,
+	CONVERT(DECIMAL (18, 2), vs.total_bytes / 1024.0 / 1024 / 1024) AS [Total Size (GB)],
+	CONVERT(DECIMAL (18, 2), vs.available_bytes / 1024.0 / 1024 / 1024) AS [Available Size (GB)],
+	CONVERT(DECIMAL (18, 2), vs.available_bytes * 1. / vs.total_bytes * 100.) AS [Space Free %]
+FROM sys.master_files AS f WITH (NOLOCK) CROSS APPLY sys.dm_os_volume_stats(f.database_id, f.[file_id]) AS vs
+ORDER BY vs.volume_mount_point
+OPTION (RECOMPILE);
